25c1c687bef2890bf9b05e22256f03aaf2e899db
murad15 <muradis1503@gmail.com>
2026-01-05 19:47:54 +0000

    gitpusher

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -5,7 +5,6 @@
 import pandas as pd
 
 
-#This prints df from dictionary
-"""This prints df from dict"""
-df = pd.DataFrame({"First" : [0.0, 0.5, 1.0, 1.5], "Second" : ["one", "two", "three", "four"]}, index = ["A", "B", "C", "D"])
-
+# This prints df from dictionary
+df = pd.DataFrame({"First": [0.0, 0.5, 1.0, 1.5],
+"Second": ["one", "two", "three", "four"]}, index = ["A", "B", "C", "D"])
