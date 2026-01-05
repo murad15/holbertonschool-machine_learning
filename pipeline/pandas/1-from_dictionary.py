8794085b2f5e0e9d6b786e239b501f7231292dd1
murad15 <muradis1503@gmail.com>
2026-01-05 19:45:21 +0000

    gitpusher

---
+++ pipeline/pandas/1-from_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This is a new function"""
+
+
+import pandas as pd
+
+
+#This prints df from dictionary
+"""This prints df from dict"""
+df = pd.DataFrame({"First" : [0.0, 0.5, 1.0, 1.5], "Second" : ["one", "two", "three", "four"]}, index = ["A", "B", "C", "D"])
+
