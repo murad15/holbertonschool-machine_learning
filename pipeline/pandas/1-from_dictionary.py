5ad063b400c48b5bdf51aa600da509950d6e7f08
murad15 <muradis1503@gmail.com>
2026-01-05 19:51:01 +0000

    gitpusher

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -6,5 +6,8 @@ import pandas as pd
 
 
 # This prints df from dictionary
-df = pd.DataFrame({"First": [0.0, 0.5, 1.0, 1.5],
-"Second": ["one", "two", "three", "four"]}, index = ["A", "B", "C", "D"])
+df = pd.DataFrame(
+    {"First": [0.0, 0.5, 1.0, 1.5],
+     "Second": ["one", "two", "three", "four"]},
+    index=["A", "B", "C", "D"]
+)
