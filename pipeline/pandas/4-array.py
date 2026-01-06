e4cc823b8107ef919af482a354ac536c00f0d505
murad15 <muradis1503@gmail.com>
2026-01-06 19:20:27 +0000

    gitpusher

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -4,6 +4,4 @@
 
 def array(df):
     """This function does something interesting"""
-    df1 = df.tail(10)
-    df1 = df1.to_numpy()
-    return df1
+    return df.loc[:, ["High", "Close"]].tail(10).to_numpy()
