1d44d0bd73eb249b32ac622b6305ee7f7c9583aa
murad15 <muradis1503@gmail.com>
2026-01-06 20:03:40 +0000

    gitpusher

--- pipeline/pandas/6-flip_switch.py
+++ pipeline/pandas/6-flip_switch.py
@@ -5,5 +5,4 @@
 def flip_switch(df):
     """asdja asdjsjda sdasd asda asd"""
 
-
-    return df.sort_values(by = "Timestamp", ascending = False).T
+    return df.sort_values(by="Timestamp", ascending=False).T
