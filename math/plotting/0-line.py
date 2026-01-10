2a542640ade47b1577374fb96be18ba7e55c9a4e
murad15 <muradis1503@gmail.com>
2026-01-10 17:17:33 +0000

    gitpusher

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """awdawd awdjkadw awdkjadk awdkjawd ak djawd akwj dkajw dajkw d"""
 import numpy as np
-import matplotlib.pyplot as  plt
+import matplotlib.pyplot as plt
 
 
 def line():
