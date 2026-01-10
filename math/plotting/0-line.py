a06e8b317be9a9472e0010fb5434c6e55ccb4e57
murad15 <muradis1503@gmail.com>
2026-01-10 17:09:38 +0000

    gitpusher

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -7,4 +7,4 @@ def line():
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.plot(y, [i for i in range(0,11)], "r")
+    return plt.plot(y, [i for i in range(0,11)], "r")
