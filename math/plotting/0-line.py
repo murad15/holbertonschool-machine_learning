46892bef1f07ff31f85d183d6eeafc26d8d3e37f
murad15 <muradis1503@gmail.com>
2026-01-10 17:16:45 +0000

    gitpusher

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+"""awdawd awdjkadw awdkjadk awdkjawd ak djawd akwj dkajw dajkw d"""
 import numpy as np
 import matplotlib.pyplot as  plt
 
 
 def line():
-
+    """awdawd q wdq  qw qew qe qw rtqer tqw"""
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
@@ -13,4 +14,3 @@ def line():
     plt.plot(x, y, 'r-')
     plt.xlim(0, 10)
     plt.show()
-
