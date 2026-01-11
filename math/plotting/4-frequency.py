8d4d8e0bc467b78173b306e076e7ebf655bdb5d8
Murad Isgandarov <71070413+murad15@users.noreply.github.com>
2026-01-11 19:15:54 +0400

    Update 4-frequency.py

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
-"""
-Module that plots a histogram of student grades
-"""
+""" 4-frequency.py """
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def frequency():
-    """
-    Plots a histogram showing the distribution of student grades
-    """
+    """ frequency """
+
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
-
     plt.figure(figsize=(6.4, 4.8))
-
-    plt.hist(student_grades, bins=10, edgecolor='black')
 
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
+    plt.axis([0, 100, 0, 30])
+    plt.xticks(ticks=[i for i in range(0, 101, 10)])
+    plt.hist(
+        student_grades,
+        bins=[i for i in range(0, 101, 10)],
+        edgecolor='black'
+    )
 
-    plt.show()
