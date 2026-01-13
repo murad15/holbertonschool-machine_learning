92ca2877dc8e1b4911f24dff591c6578e9e681f8
murad15 <muradis1503@gmail.com>
2026-01-13 07:01:11 +0000

    gitpusher

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Very interesting function omg"""
+
+
+def summation_i_squared(n):
+    """This function does something interesting"""
+
+    try:
+        res = n*(n+1)*(2*n+1)/6
+        return res
+    except:
+        print("Invalid number")
