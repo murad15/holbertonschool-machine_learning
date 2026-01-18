5b7666192a0ca92eca0742592f2b188a92614c92
murad15 <muradis1503@gmail.com>
2026-01-18 10:50:49 +0000

    gitpusher

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -5,12 +5,24 @@
 def poly_derivative(poly):
     """This functions outputs derivative coefficients"""
 
-    for i in poly:
-        if isinstance(i, int) or isinstance(i, float):
-            pass
-        else:
+    # Validate input: must be a non-empty list of numbers (int/float)
+    if not isinstance(poly, list) or len(poly) == 0:
+        return None
+
+    for c in poly:
+        # Reject non-numeric coefficients (also rejects bool, since bool is a subclass of int)
+        if isinstance(c, bool) or not isinstance(c, (int, float)):
             return None
-    coefs = []
-    for q in range(1, len(poly)):
-        coefs.append(poly[q]*q)
-    return coefs
+
+    # Derivative of a constant (degree 0) is 0
+    if len(poly) == 1:
+        return [0]
+
+    # Build derivative: d/dx (c_i * x^i) = i * c_i * x^(i-1) for i >= 1
+    deriv = [i * poly[i] for i in range(1, len(poly))]
+
+    # If all coefficients are zero, derivative is zero polynomial
+    if all(v == 0 for v in deriv):
+        return [0]
+
+    return deriv
