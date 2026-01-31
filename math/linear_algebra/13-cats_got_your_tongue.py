ffbcc3f01407ec5bf0ad9f82a407bbff214ae94c
murad15 <muradis1503@gmail.com>
2026-01-31 19:41:56 +0000

    gitpusher

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Something that function does"""
+
+
+def np_cat(mat1, mat2, axis=0):
+    """Something that function does"""
+
+    return np.concatenate((mat1, mat2), axis=axis)
