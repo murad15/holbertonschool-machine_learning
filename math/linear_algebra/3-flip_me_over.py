ac000ec9eafae0a0bd8bca5d9ffa7dd8e8d850ea
murad15 <muradis1503@gmail.com>
2026-01-31 18:40:10 +0000

    gitpusher

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""wqeqwf fqw fw qwf wf wqf qwf qw"""
+
+
+def matrix_transpose(matrix):
+    """qwe qw q e qwe qwe qwe"""
+
+    return [[matrix[row][col] for row in range(len(matrix))] for col in range(len(matrix[0]))]
