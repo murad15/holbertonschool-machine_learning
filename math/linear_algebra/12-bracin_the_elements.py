b544623be97126216503d584cd490ec911c86281
murad15 <muradis1503@gmail.com>
2026-01-31 19:40:20 +0000

    gitpusher

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -5,6 +5,4 @@
 def np_elementwise(mat1, mat2):
     """Something that function does"""
 
-    mat1 = np.array(mat1)
-    mat2 = np.array(mat2)
     return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
