c9b44fb1c99dae58822d2392106c885d14c17662
murad15 <muradis1503@gmail.com>
2026-01-31 18:49:22 +0000

    gitpusher

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -9,5 +9,5 @@ def add_arrays(arr1, arr2):
         return None
     result=[]
     for i in range(len(arr1)):
-        result.append(arr1[i]+arr2[i])
+        result.append(arr1[i] + arr2[i])
     return result
