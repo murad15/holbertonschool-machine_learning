70e7a112e7ed8febe57f4ce823c5d30190476318
murad15 <muradis1503@gmail.com>
2026-01-31 18:47:47 +0000

    gitpusher

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""qwe we qe wq eqw ewqeqewq"""
+
+
+def add_arrays(arr1, arr2):
+    """qwqwe qweqweqweqweqwewqe  qwe qwqw e eqweqwe"""
+
+    if len(arr1)!=len(arr2):
+        return None
+    result=[]
+    for i in range(len(arr1)):
+        result.append(arr1[i]+arr2[i])
+    return result
