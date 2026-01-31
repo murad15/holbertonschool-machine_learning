e22f627ad5b885dc6ee351670633ce290f4d21b1
murad15 <muradis1503@gmail.com>
2026-01-31 19:46:40 +0000

    gitpusher

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Something that function does"""
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Something that function does"""
+
+    return np.matmul(mat1, mat2)
