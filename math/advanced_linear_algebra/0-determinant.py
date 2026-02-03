0a7c158d8438ca84d0ebf3ea467549740c2e30ad
murad15 <muradis1503@gmail.com>
2026-02-03 20:41:20 +0000

    gitpusher

---
+++ math/advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Something that function does"""
+
+
+def determinant(matrix):
+    """Something that function does"""
+
+    if not isinstance(matrix, list) or not isinstance(matrix[0], list):
+        raise TypeError("matrix must be a list of lists")
+    if matrix == [[]]:
+        return 1
+    try:
+        det = np.linalg.det(matrix)
+        return det
+    except ValueError:
+        print("matrix must be a square matrix")
