efa852d01b93a8a713731d4a23cf1a1e25fffc87
murad15 <muradis1503@gmail.com>
2026-02-16 09:11:55 +0000

    gitpusher

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -8,10 +8,13 @@ import numpy as np
 def create_confusion_matrix(labels, logits):
     """Something that function does"""
 
-    labs = np.argmax(labels, axis=1)
-    logs = np.argmax(logits, axis=1)
+    true_cls = np.argmax(labels, axis=1)
+    pred_cls = np.argmax(logits, axis=1)
 
-    matrix = np.zeros(shape=(labels.shape[1], labels.shape[1]))
-    for i in labs:
-        matrix[labs[i],logs[i]] += 1
-    return matrix
+    classes = labels.shape[1]
+    confusion = np.zeros((classes, classes), dtype=int)
+
+    for i in range(labels.shape[0]):   # iterats
+        confusion[true_cls[i], pred_cls[i]] += 1
+
+    return confusion
