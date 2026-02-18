b636ccb02219cd7f7bec2f5d3def1ea56c05cfef
murad15 <muradis1503@gmail.com>
2026-02-18 14:29:47 +0000

    gitpusher

---
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Something that function does"""
+
+
+import numpy as np
+
+
+def sensitivity(confusion):
+    """Something that function does"""
+    z = 0
+    c=[]
+    for i in range(confusion.shape[0]):
+        sens = float(confusion[i][z]/np.sum(a[i,:]))
+        c.append(sens)
+        z += 1
+    c = np.array(c)
+    return c
