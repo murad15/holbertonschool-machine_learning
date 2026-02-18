625890ae11c972512eb2d6d8f270de209faf3f1e
murad15 <muradis1503@gmail.com>
2026-02-18 14:39:21 +0000

    gitpusher

---
+++ supervised_learning/error_analysis/2-precision.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Something that function does"""
+
+
+import numpy as np
+
+
+def precision(confusion):
+    """Something that function does"""
+    z = 0
+    c = []
+    for i in range(confusion.shape[0]):
+        sens = float(confusion[i][z]/np.sum(confusion[:, i]))
+        c.append(sens)
+        z += 1
+    c = np.array(c)
+    return c
