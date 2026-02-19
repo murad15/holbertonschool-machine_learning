2c710ebbc56d196de902f5841cd69c9251eddab4
murad15 <muradis1503@gmail.com>
2026-02-19 14:35:53 +0000

    gitpusher

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+"""Qq  w eq eqwqw few fefwef ewf"""
 
 
 import numpy as np
 
 
 class Node:
+    """qwdwdq wdq qwd w dqw w"""
     def __init__(self, feature=None, threshold=None, left_child=None, right_child=None, is_root=False, depth=0):
         self.feature = feature
         self.threshold = threshold
@@ -15,11 +17,13 @@ class Node:
         self.sub_population = None
         self.depth = depth
 
-    def max_depth_below(self) :
-        return self.depth
+    def max_depth_below(self):
+        """qwdwdq wdq qwd w dqw w"""
+        return max(self.left_child.max_depth_below(), self.right_child.max_depth_below())
 
 
 class Leaf(Node):
+    """qwdwdq wdq qwd w dqw w"""
     def __init__(self, value, depth=None):
         super().__init__()
         self.value = value
@@ -27,18 +31,12 @@ class Leaf(Node):
         self.depth = depth
 
     def max_depth_below(self):
-        """
-        Recursively finds the maximum depth of the tree 
-        starting from this node.
-        """
-        # Recursively get the max depth from the left and right subtrees
-        left_max = self.left_child.max_depth_below()
-        right_max = self.right_child.max_depth_below()
+        """qwdwdq wdq qwd w dqw w"""
+        return self.depth
 
-        # Return the greater of the two
-        return max(left_max, right_max)
 
 class Decision_Tree():
+    """qwdwdq wdq qwd w dqw w"""
     def __init__(self, max_depth=10, min_pop=1, seed=0, split_criterion="random", root=None):
         self.rng = np.random.default_rng(seed)
         if root:
@@ -52,5 +50,6 @@ class Decision_Tree():
         self.split_criterion = split_criterion
         self.predict = None
 
-    def depth(self) :
+    def depth(self):
+        """qwdwdq wdq qwd w dqw w"""
         return self.root.max_depth_below()
