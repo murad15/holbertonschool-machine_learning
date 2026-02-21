b482716aa3d06a4b8037e0e4d81d29d8df397438
murad15 <muradis1503@gmail.com>
2026-02-21 21:44:14 +0000

    gitpusher

--- supervised_learning/decision_tree/11-isolation_forest.py
+++ supervised_learning/decision_tree/11-isolation_forest.py
@@ -26,16 +26,16 @@ class Isolation_Random_Forest():
         predictions = np.array([f(explanatory) for f in self.numpy_preds])
         return predictions.mean(axis=0)
 
-    def fit(self,explanatory, n_trees=100, verbose=0):
+    def fit(self, explanatory, n_trees=100, verbose=0):
         """Something that function does"""
         self.explanatory = explanatory
         self.numpy_preds = []
         depths = []
         nodes = []
         leaves = []
+        max = self.max_depth
         for i in range(n_trees):
-            T = Isolation_Random_Tree(max_depth=self.max_depth,
-                                       seed=self.seed+i)
+            T = Isolation_Random_Tree(max_depth=max, seed=self.seed+i)
             T.fit(explanatory)
             self.numpy_preds.append(T.predict)
             depths.append(T.depth())
