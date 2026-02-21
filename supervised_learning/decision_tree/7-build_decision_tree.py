76f479a216716c7df37ae89ccc6ff24d8fe00fd5
murad15 <muradis1503@gmail.com>
2026-02-21 20:37:11 +0000

    gitpusher

--- supervised_learning/decision_tree/7-build_decision_tree.py
+++ supervised_learning/decision_tree/7-build_decision_tree.py
@@ -248,11 +248,11 @@ class Decision_Tree:
         self.update_predict()
 
         if verbose == 1:
-            print(f"""Training finished.
-- Depth                     : {self.depth()}
-- Number of nodes           : {self.count_nodes()}
-- Number of leaves          : {self.count_nodes(only_leaves=True)}
-- Accuracy on training data : {self.accuracy(self.explanatory, self.target)}""")
+            print(f"Training finished.\n"
+                  f"- Depth                     : {self.depth()}\n"
+                  f"- Number of nodes           : {self.count_nodes()}\n"
+                  f"- Number of leaves          : {self.count_nodes(only_leaves=True)}\n"
+                  f"- Accuracy on training data : {self.accuracy(self.explanatory, self.target)}")
 
     def accuracy(self, test_explanatory, test_target):
         """Calculates model accuracy."""
