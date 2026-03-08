241ec86b36bcf6541e2eb12c912b00bd505df84b
murad15 <muradis1503@gmail.com>
2026-03-08 20:22:43 +0000

    gitpusher

---
+++ supervised_learning/classification/2-neuron.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+
+import numpy as np
+
+
+class Neuron:
+    """Class that defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """Initialize the neuron"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        # Private attributes
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for weights"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for activated output"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """
+        Calculates forward propagation using sigmoid activation
+        X: numpy.ndarray with shape (nx, m)
+        """
+        Z = np.dot(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A 
