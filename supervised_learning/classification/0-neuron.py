d96c1a0021ad77ca65323296bdd795179d32a29c
murad15 <muradis1503@gmail.com>
2026-03-08 19:32:34 +0000

    gitpusher

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Something that function does"""
+
+import numpy as np
+
+class Neuron:
+    """Something that function does"""
+
+    def __init__(self, nx):
+        if not isinstance(self.nx, int):
+            raise TypeError("nx must be an integer")
+        if self.nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        # Weights initialized with random normal distribution
+        self.W = np.random.randn(1, nx)
+
+        # Bias initialized to 0
+        self.b = 0
+
+        # Activated output initialized to 0
+        self.A = 0
---
+++ supervised_learning/classification/README.md
@@ -0,0 +1,1 @@
+Supervised learning classification task using NN
