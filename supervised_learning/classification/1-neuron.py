89ac550b2d516ab81923f58474e6a7c4521a364b
murad15 <muradis1503@gmail.com>
2026-03-08 20:09:04 +0000

    gitpusher

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -13,15 +13,6 @@ class Neuron:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        # Weights initialized with random normal distribution
-        self.W = np.random.randn(1, nx)
-
-        # Bias initialized to 0
-        self.b = 0
-
-        # Activated output initialized to 0
-        self.A = 0
-
         self.__W = np.random.randn(1, nx)
 
         self.__b = 0
