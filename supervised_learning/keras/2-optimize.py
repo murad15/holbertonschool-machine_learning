68a42ea5636895baf4e4f22ad4c0e3e95bf5df9c
murad15 <muradis1503@gmail.com>
2026-03-10 11:31:58 +0000

    gitpusher

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -9,8 +9,11 @@ def optimize_model(network, alpha, beta1, beta2):
     """Something that function does"""
 
     opt = K.optimizers.Adam(
-        learning_rate = alpha,
-        beta_1 = beta1,
-        beta_2 = beta2
+        learning_rate=alpha,
+        beta_1=beta1,
+        beta_2=beta2
     )
-    network.compile(optimizer=opt, loss="categorical_crossentropy", metrics=["accuracy"])
+    network.compile(
+        optimizer=opt,
+        loss="categorical_crossentropy",
+        metrics=["accuracy"])
