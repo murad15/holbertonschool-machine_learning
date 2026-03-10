fb07c776e1c0229b8cfd30d4c79275bbb5f0657e
murad15 <muradis1503@gmail.com>
2026-03-10 10:29:11 +0000

    gitpusher

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -8,25 +8,27 @@ import tensorflow.keras as K
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """Qweqe qweqw eqweqw dasdsad """
 
+    model = K.Sequential()
     regularizer = K.regularizers.l2(lambtha)
 
-    x = K.layers.Dense(
-        layers[0],
-        activation=activations[0],
-        kernel_regularizer=regularizer,
-        input_shape=(nx,)
-    )
+    for i in range(len(layers)):
+        if i == 0:
+            model.add(
+                K.layers.Dense(
+                    units=layers[i],
+                    activation=activations[i],
+                    kernel_regularizer=regularizer,
+                    input_shape=(nx,)
+                )
+            )
+        else:
+            model.add(K.layers.Dropout(rate=1 - keep_prob))
+            model.add(
+                K.layers.Dense(
+                    units=layers[i],
+                    activation=activations[i],
+                    kernel_regularizer=regularizer
+                )
+            )
 
-    inputs = x.input
-    outputs = x.output
-
-    for i in range(1, len(layers)):
-        outputs = K.layers.Dropout(1 - keep_prob)(outputs)
-        outputs = K.layers.Dense(
-            layers[i],
-            activation=activations[i],
-            kernel_regularizer=regularizer
-        )(outputs)
-
-    model = K.Model(inputs=inputs, outputs=outputs)
     return model
