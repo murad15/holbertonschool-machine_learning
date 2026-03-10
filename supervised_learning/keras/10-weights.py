d4e9b52d1776a43ed0ec75d49ea7616aff0d842c
murad15 <muradis1503@gmail.com>
2026-03-10 21:02:52 +0000

    gitpusher

--- supervised_learning/keras/10-weights.py
+++ supervised_learning/keras/10-weights.py
@@ -8,7 +8,7 @@ import tensorflow.keras as K
 def save_weights(network, filename, save_format='keras'):
     """Something that function does"""
 
-    network.save_weights(filename, save_format = save_format)
+    network.save_weights(filename, save_format=save_format)
 
 def load_weights(network, filename):
     """Something that function does"""
