dfd9bb3fc825aad75f4b618a03f1fd1f14f2f247
murad15 <muradis1503@gmail.com>
2026-03-10 11:22:54 +0000

    gitpusher

---
+++ supervised_learning/keras/2-optimize.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Something that function does"""
+
+
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+"""Something that function does"""
+
+    opt = tf.keras.optimizers.Adam(
+        learning_rate = alpha,
+        beta_1 = beta1,
+        beta_2 = beta2
+    )
+    model.compile(optimizer=opt)
