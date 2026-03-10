25629fee0511693ac78e973335c65134dbc74502
murad15 <muradis1503@gmail.com>
2026-03-10 21:33:40 +0000

    gitpusher

---
+++ supervised_learning/keras/12-test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Something that function does"""
+
+
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels, verbose=True):
+    """Something that function does"""
+
+    loss, accuracy = network.evaluate(x=data, y=labels, verbose=verbose)
+
+    return loss, accuracy
