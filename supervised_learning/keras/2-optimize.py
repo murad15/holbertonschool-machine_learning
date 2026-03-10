c761024850d0964b19eb0a176be1b2fad435dbde
murad15 <muradis1503@gmail.com>
2026-03-10 11:26:59 +0000

    gitpusher

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -6,11 +6,11 @@ import tensorflow.keras as K
 
 
 def optimize_model(network, alpha, beta1, beta2):
-"""Something that function does"""
+    """Something that function does"""
 
     opt = tf.keras.optimizers.Adam(
         learning_rate = alpha,
         beta_1 = beta1,
         beta_2 = beta2
     )
-    model.compile(optimizer=opt, loss="categorical_crossentropy", metrics=["accuracy"])
+    network.compile(optimizer=opt, loss="categorical_crossentropy", metrics=["accuracy"])
