528e2c01c440cae817d8b933ff385d99c34dada3
murad15 <muradis1503@gmail.com>
2026-03-10 21:18:06 +0000

    gitpusher

--- supervised_learning/keras/11-config.py
+++ supervised_learning/keras/11-config.py
@@ -8,11 +8,14 @@ import tensorflow.keras as K
 def save_config(network, filename):
     """Something that function does"""
 
-    network.to_json(filename)
-
+    with open(filename, 'w') as f:
+        f.write(network.to_json())
 
 def load_config(network, filename):
     """Something that function does"""
 
-    network = K.models.model_from_json(filename)
-    return network
+    with open(filename, 'r') as f:
+        json_config = f.read()
+
+    model = K.models.model_from_json(json_config)
+    return model
