1ba021458046125b1bd3c5fbb41a30a1034ac638
murad15 <muradis1503@gmail.com>
2026-03-10 21:05:32 +0000

    gitpusher

---
+++ supervised_learning/keras/11-config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Something that function does"""
+
+
+import tensorflow.keras as K
+
+
+def save_config(network, filename):
+    """Something that function does"""
+
+    network.save_config(filename)
+
+
+def load_config(network, filename):
+    """Something that function does"""
+
+    network.load_config(filename)
