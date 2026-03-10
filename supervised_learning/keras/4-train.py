e2ab3ca963585f3b94bc4000460e2010b2e4c2cf
murad15 <muradis1503@gmail.com>
2026-03-10 13:31:10 +0000

    gitpusher

---
+++ supervised_learning/keras/4-train.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Something that function does"""
+
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
+    """Something that function does"""
+
+    history = network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle
+    )
+
+    return history
+
