91e83da312d3168e948edd8c4be58c7f44047e5c
murad15 <muradis1503@gmail.com>
2026-03-11 14:42:42 +0000

    gitpusher

---
+++ supervised_learning/optimization/0-norm_constants.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Something that function does"""
+
+
+import numpy as np
+
+
+def normalization_constants(X):
+    """qweq eqweqwe qwe dq wewf wef"""
+
+    m = np.mean(X, axis = 1)
+    s = np.std(X, axis = 1)
+    return m, s
---
+++ supervised_learning/optimization/README.md
@@ -0,0 +1,1 @@
+Optimization project using tensorflow and numpy
