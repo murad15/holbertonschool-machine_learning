2252a467df642e4797122088b82bb75d3e584264
murad15 <muradis1503@gmail.com>
2026-03-12 20:35:46 +0000

    gitpusher

---
+++ supervised_learning/optimization/10-Adam.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Something that function does"""
+
+import tensorflow as tf
+
+
+def create_Adam_op(alpha, beta1, beta2, epsilon):
+    """Something that function does"""
+
+    opt = tf.keras.optimizers.Adam(
+             learning_rate=alpha,
+             beta_1=beta1,
+             beta_2=beta2,
+             epsilon=epsilon)
+    return opt
