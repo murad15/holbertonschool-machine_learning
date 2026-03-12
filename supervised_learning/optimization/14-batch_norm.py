340e5388ecc7ced8860587b01c0e45e4c57a7bb1
murad15 <muradis1503@gmail.com>
2026-03-12 21:10:23 +0000

    gitpusher

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -8,33 +8,23 @@ import tensorflow as tf
 def create_batch_norm_layer(prev, n, activation):
     """Something that function does"""
 
-    init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
 
-    # 2. Create the Dense base layer
-    # Note: use_bias=False is common with Batch Norm because 
-    # acts as the bias, making a separate bias term redundant.
-    dense_layer = tf.keras.layers.Dense(
+    # Dense layer (without activation yet)
+    dense = tf.keras.layers.Dense(
         units=n,
-        kernel_initializer=init,
+        kernel_initializer=initializer,
         use_bias=False
-    )
+    )(prev)
 
-    # Pass the previous layer output through the Dense layer
-    z = dense_layer(prev)
-
-    # 3. Create the Batch Normalization layer
-    # gamma_init='onesbeta_init='zeros' are the defaults in Keras
+    # Batch normalization layer
     batch_norm = tf.keras.layers.BatchNormalization(
         epsilon=1e-7,
-        beta_initializer="zeros",
-        gamma_initializer="ones"
-    )
+        gamma_initializer=tf.keras.initializers.Ones(),
+        beta_initializer=tf.keras.initializers.Zeros()
+    )(dense)
 
-    # Apply Batch Normalization to the linear output z
-    z_norm = batch_norm(z)
+    # Apply activation
+    output = activation(batch_norm)
 
-    # 4. Apply the activation function
-    # If activationit returns the normalized linear output
-    if activation is None:
-        return z_norm
-    return activation(z_norm)
+    return output
