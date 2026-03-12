6338e20c7b193afe3889bace56327c37b9931f5d
murad15 <muradis1503@gmail.com>
2026-03-12 12:54:00 +0000

    gitpusher

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -5,4 +5,12 @@
 def moving_average(data, beta):
     """Something that function does"""
 
-    return data*beta*(1/(1-beta))
+    averages = []
+    v = 0
+
+    for i, x in enumerate(data, 1):
+        v = beta * v + (1 - beta) * x
+        v_corrected = v / (1 - beta ** i)
+        averages.append(v_corrected)
+
+    return averages
