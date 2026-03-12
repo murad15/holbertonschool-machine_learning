835e32c881e04f6ebdbd88f83cc9e32d290211b4
murad15 <muradis1503@gmail.com>
2026-03-12 19:45:33 +0000

    gitpusher

--- supervised_learning/optimization/7-RMSProp.py
+++ supervised_learning/optimization/7-RMSProp.py
@@ -10,6 +10,6 @@ def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
 
     e_s = beta2 * s + (1 - beta2) * (grad ** 2)
 
-    var_n = var - alpha * (grad / (np.sqrt(e_s + epsilon))
+    var_n = var - (alpha / (np.sqrt(e_s + epsilon)) * grad
 
     return var_n, e_s
