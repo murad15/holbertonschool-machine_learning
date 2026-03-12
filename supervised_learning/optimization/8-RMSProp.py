3933afec1f917aeecf824e782ec5b68ba50c294d
murad15 <muradis1503@gmail.com>
2026-03-12 20:14:08 +0000

    gitpusher

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Something that function does"""
+
+
+import tensorflow as tf
+
+
+def create_RMSProp_op(alpha, beta2, epsilon):
+    """Something that function does"""
+
+    opt = tf.keras.optimizers.RMSprop(
+             learning_rate=alpha,
+             momentum=beta2,
+             epsilon=epsilon)
+    return opt
