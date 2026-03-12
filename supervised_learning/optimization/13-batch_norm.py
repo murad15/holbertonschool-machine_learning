fc7d7fbeef970832bc4fbc6ae4da2977c9e2b46c
murad15 <muradis1503@gmail.com>
2026-03-12 21:01:52 +0000

    gitpusher

--- supervised_learning/optimization/13-batch_norm.py
+++ supervised_learning/optimization/13-batch_norm.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+\#!/usr/bin/env python3
 """Something that function does"""
 
 
@@ -8,6 +8,6 @@ import numpy as np
 def batch_norm(Z, gamma, beta, epsilon):
     """Something that function does"""
 
-    Z_n = ((Z + beta) / (gamma + epsilon))
+    Z_n = Z * gamma + beta
 
     return Z_n
