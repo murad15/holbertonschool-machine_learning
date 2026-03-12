0b22cb10eb9feb92448cc24ea9fe628e3479f828
murad15 <muradis1503@gmail.com>
2026-03-12 13:48:12 +0000

    gitpusher

---
+++ supervised_learning/optimization/5-momentum.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Something that function does"""
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """Something that function does"""
+
+    v1 = beta1 * v + (1 - beta1) * grad
+
+    var_n = var - alpha * v1
+
+    return var_n, v1 
