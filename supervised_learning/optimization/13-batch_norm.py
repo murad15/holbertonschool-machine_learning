3c29d6d4dd86e39d02f0cbeeb0878f393f08f677
murad15 <muradis1503@gmail.com>
2026-03-12 21:03:48 +0000

    gitpusher

--- supervised_learning/optimization/13-batch_norm.py
+++ supervised_learning/optimization/13-batch_norm.py
@@ -8,6 +8,18 @@ import numpy as np
 def batch_norm(Z, gamma, beta, epsilon):
     """Something that function does"""
 
-    Z_n = Z * gamma + beta
+    # 1. Calculate the mean along the batch (m data points)
+    # axis=0 calculates the mean for each feature (n)
+    mu = np.mean(Z, axis=0)
 
-    return Z_n
+    # 2. Calculate the variance along the batch
+    sigma_squared = np.var(Z, axis=0)
+
+    # 3. Normalize:n and divide by standard deviation
+    # Z_hat = (Z - mu) / sqrt(sigma^2 + epsilon)
+    Z_hat = (Z - mu) / np.sqrt(sigma_squared + epsilon)
+
+    # 4. Scale and shift (Gamma and Beta)
+    Z_norm = gamma * Z_hat + beta
+
+    return Z_norm
