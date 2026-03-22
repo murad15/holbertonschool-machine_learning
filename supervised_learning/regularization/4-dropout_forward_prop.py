d0da6b1cc59fb2416f62eb8908bca452c2730b7b
murad15 <muradis1503@gmail.com>
2026-03-22 18:58:07 +0000

    gitpusher

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -7,23 +7,27 @@ import numpy as np
 
 def dropout_forward_prop(X, weights, L, keep_prob):
     """Forward propagation using Dropout"""
-    cache = {"A0": X}
+    cache = {}
+    cache['A0'] = X
 
     for i in range(1, L + 1):
-        W = weights["W" + str(i)]
-        b = weights["b" + str(i)]
-        A_prev = cache["A" + str(i - 1)]
+        W = weights[f'W{i}']
+        b = weights[f'b{i}']
+        A_prev = cache[f'A{i-1}']
+
+        # Linear Step
         Z = np.matmul(W, A_prev) + b
 
-        if i == L:
-            exp = np.exp(Z - np.max(Z, axis=0, keepdims=True))
-            A = exp / np.sum(exp, axis=0, keepdims=True)
-        else:
+        if i < L:
+            # Tanh activation for hidden layers
             A = np.tanh(Z)
-            D = np.random.rand(*A.shape) < keep_prob
-            A = (A * D) / keep_prob
-            cache["D" + str(i)] = D
-
-        cache["A" + str(i)] = A
+            mask = (np.random.rand(A.shape[0], A.shape[1]) < keep_prob).astype(int)
+            A = (A * mask) / keep_prob
+            cache[f'D{i}'] = mask
+            cache[f'A{i}'] = A
+        else:
+            exp_Z = np.exp(Z - np.max(Z, axis=0, keepdims=True))
+            A = exp_Z / np.sum(exp_Z, axis=0, keepdims=True)
+            cache[f'A{i}'] = A
 
     return cache
