7936290060d885b2ed93b7a05ebd30aa71201d2d
murad15 <muradis1503@gmail.com>
2026-03-22 18:55:25 +0000

    gitpusher

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -5,31 +5,25 @@
 import numpy as np
 
 
-def softmax(Z):
-    """Compute softmax"""
-    exp = np.exp(Z - np.max(Z, axis=0, keepdims=True))
-    return exp / np.sum(exp, axis=0, keepdims=True)
-
 def dropout_forward_prop(X, weights, L, keep_prob):
-    """Forward propagation with Dropout"""
-
-    cache = {}
-    cache["A0"] = X
+    """Forward propagation using Dropout"""
+    cache = {"A0": X}
 
     for i in range(1, L + 1):
         W = weights["W" + str(i)]
         b = weights["b" + str(i)]
         A_prev = cache["A" + str(i - 1)]
-
         Z = np.matmul(W, A_prev) + b
+
         if i == L:
-            A = softmax(Z)
+            exp = np.exp(Z - np.max(Z, axis=0, keepdims=True))
+            A = exp / np.sum(exp, axis=0, keepdims=True)
         else:
             A = np.tanh(Z)
-
             D = np.random.rand(*A.shape) < keep_prob
             A = (A * D) / keep_prob
             cache["D" + str(i)] = D
 
         cache["A" + str(i)] = A
+
     return cache
