ed02cf2bf161c1d292834eb3d32455888e54ea27
murad15 <muradis1503@gmail.com>
2026-03-28 20:37:03 +0000

    gitpusher

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -6,34 +6,32 @@ import numpy as np
 
 
 def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
-    """Something that function does"""
-
+    """
+    Performs a convolution on multiple grayscale images.
+    """
     m, h, w = images.shape
     kh, kw = kernel.shape
+    sh, sw = stride
+
     if padding == 'same':
-        ph, pw = kh // 2, kw // 2
+        ph = ((h - 1) * sh + kh - h) // 2 + 1
+        pw = ((w - 1) * sw + kw - w) // 2 + 1
     elif padding == 'valid':
         ph, pw = 0, 0
     else:
         ph, pw = padding
-    sh, sw = stride
 
-    output_h = int(np.floor((h + 2*ph - kh) / sh) + 1)
-    output_w = int(np.floor((w + 2*pw - kw) / sw) + 1)
-    if padding != 'valid':
-        padded = np.pad(
-            images,
-            ((0, 0), (ph, ph), (pw, pw)),
-            mode='constant'
-        )
-    else:
-        padded = images
+    images_padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw)),
+                           mode='constant', constant_values=0)
 
-    output = np.zeros((m, output_h, output_w))
+    oh = (h + 2 * ph - kh) // sh + 1
+    ow = (w + 2 * pw - kw) // sw + 1
 
-    for i in range(output_h):
-        for j in range(output_w):
-            window = padded[:, i * sh:i * sh + kh, j * sw:j * sw + kw]
-            output[:, i, j] = np.sum(window * kernel, axis=(1, 2))
+    output = np.zeros((m, oh, ow))
+
+    for i in range(oh):
+        for j in range(ow):
+            img_slice = images_padded[:, i*sh:i*sh+kh, j*sw:j*sw+kw]
+            output[:, i, j] = np.sum(img_slice * kernel, axis=(1, 2))
 
     return output
