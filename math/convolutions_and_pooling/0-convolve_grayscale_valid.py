7dea0b775aaebf2b4cb692b338077ef6f6304275
murad15 <muradis1503@gmail.com>
2026-03-28 18:40:30 +0000

    gitpusher

---
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Something that function does"""
+
+
+import numpy as np
+
+
+def convolve_grayscale_valid(images, kernel):
+    """Something that function does"""
+
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+
+
+    output_h = h - kh + 1
+    output_w = w - kw + 1
+
+    output = np.zeros((m, output_h, output_w))
+
+    for i in range(output_h):
+        for j in range(output_w):
+            window = images[:, i:i + kh, j:j + kw]
+            output[:, i, j] = np.sum(window * kernel, axis=(1, 2))
+
+    return output
---
+++ math/convolutions_and_pooling/README.md
@@ -0,0 +1,1 @@
+Introduction to Convolutional Neural Netvorks: Convolutions and Pooling
