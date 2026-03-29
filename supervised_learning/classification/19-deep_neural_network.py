8a78a13e8eb7986d19254b6e6c3015864b55aef4
murad15 <muradis1503@gmail.com>
2026-03-29 23:17:04 +0000

    gitpusher

---
+++ supervised_learning/classification/19-deep_neural_network.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+"""Module for DeepNeuralNetwork class with private attributes"""
+
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """Defines a deep neural network performing binary classification"""
+
+    def __init__(self, nx, layers):
+        """Class constructor"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list):
+            raise TypeError("layers must be a list of positive integers")
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+
+        for i in range(self.__L):
+            if not isinstance(layers[i], int) or layers[i] <= 0:
+                raise TypeError(
+                    "layers must be a list of positive integers"
+                )
+
+            n_curr = layers[i]
+            n_prev = nx if i == 0 else layers[i - 1]
+
+            self.__weights["W{}".format(i + 1)] = (
+                np.random.randn(n_curr, n_prev) *
+                np.sqrt(2 / n_prev)
+            )
+            self.__weights["b{}".format(i + 1)] = np.zeros((n_curr, 1))
+
+    @property
+    def L(self):
+        """Getter for number of layers"""
+        return self.__L
+
+    @property
+    def cache(self):
+        """Getter for cache"""
+        return self.__cache
+
+    @property
+    def weights(self):
+        """Getter for weights"""
+        return self.__weights
+
+    def forward_prop(self, X):
+        """Calculates forward propagation of the neural network"""
+        self.__cache["A0"] = X
+
+        for i in range(1, self.__L + 1):
+            W = self.__weights["W{}".format(i)]
+            b = self.__weights["b{}".format(i)]
+            A_prev = self.__cache["A{}".format(i - 1)]
+
+            Z = np.matmul(W, A_prev) + b
+            A = 1 / (1 + np.exp(-Z))
+
+            self.__cache["A{}".format(i)] = A
+
+        return A, self.__cache
+
+    def cost(self, Y, A):
+        """Calculates the cost using logistic regression"""
+        m = Y.shape[1]
+        cost = -np.sum(Y * np.log(A) +
+                   (1 - Y) * np.log(1.0000001 - A)) / m
+        return cost
