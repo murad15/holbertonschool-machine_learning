ff64612bb4847578ef6e709e75e88f0815f086cf
murad15 <muradis1503@gmail.com>
2026-03-29 18:43:06 +0000

    gitpusher

---
+++ math/convolutions_and_pooling/6-pool.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Something that function does"""
+
+
+import numpy as np
+
+
+def pool(images, kernel_shape, stride, mode='max'):
+    """Something that function does"""
+
+    m, h, w, c = images.shape
+    kh, kw = kernel_shape
+    sh, sw = stride
+
+    images
+    oh = (h - kh) // sh + 1
+    ow = (w - kw) // sw + 1
+
+    output = np.zeros((m, oh, ow, nc))
+
+    for i in range(oh):
+        for j in range(ow):
+            img_slice = images[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :]
+            if mode = 'max':
+                output[:, i, j, k] = np.max(img_slice, axis=(1,2))
+            else:
+                output[:, i, j, k] = np.mean(img_slice, axis=(1,2))
+
+
+    return output
