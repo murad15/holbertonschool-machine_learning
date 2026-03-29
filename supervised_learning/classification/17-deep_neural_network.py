6cacc9b13092678c97c82f4f24e2ef7b7ac8adf1
murad15 <muradis1503@gmail.com>
2026-03-29 23:04:08 +0000

    gitpusher

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -9,16 +9,12 @@ class DeepNeuralNetwork:
     """Defines a deep neural network performing binary classification"""
 
     def __init__(self, nx, layers):
-        """
-        Initializes the deep neural network
-        nx: number of input features
-        layers: list representing the nodes in each layer
-        """
+        """Class constructor"""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or len(layers) == 0:
+        if not isinstance(layers, list):
             raise TypeError("layers must be a list of positive integers")
 
         self.__L = len(layers)
@@ -26,33 +22,31 @@ class DeepNeuralNetwork:
         self.__weights = {}
 
         for i in range(self.__L):
-            # Check if layer nodes are positive integers inside the loop
             if not isinstance(layers[i], int) or layers[i] <= 0:
-                raise TypeError("layers must be a list of positive integers")
+                raise TypeError(
+                    "layers must be a list of positive integers"
+                )
 
-            # Determine dimensions for weight matrices
-            # Layer 1 uses nx; subsequent layers use the size of the previous layer
-            n_prev = nx if i == 0 else layers[i - 1]
             n_curr = layers[i]
+            n_prev = nx if i == 0 else layers[i - 1]
 
-            # He et al. initialization
             self.__weights["W{}".format(i + 1)] = (
-                np.random.randn(n_curr, n_prev) * np.sqrt(2 / n_prev)
+                np.random.randn(n_curr, n_prev) *
+                np.sqrt(2 / n_prev)
             )
-            # Bias initialization to 0
             self.__weights["b{}".format(i + 1)] = np.zeros((n_curr, 1))
 
     @property
     def L(self):
-        """Getter for the number of layers"""
+        """Getter for number of layers"""
         return self.__L
 
     @property
     def cache(self):
-        """Getter for the intermediary values cache"""
+        """Getter for cache"""
         return self.__cache
 
     @property
     def weights(self):
-        """Getter for the weights dictionary"""
+        """Getter for weights"""
         return self.__weights
