9659f92c1716b90dcc68ba9e114bb6d1155a83d0
murad15 <muradis1503@gmail.com>
2026-03-29 22:54:36 +0000

    gitpusher

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -6,16 +6,19 @@ import numpy as np
 
 
 class DeepNeuralNetwork:
-    """Deep neural network perming binary classification"""
+    """Defines a deep neural network performing binary classification"""
 
     def __init__(self, nx, layers):
+        """
+        Class constructor
+        nx: number of input features
+        layers: list representing the number of nodes in each layer
+        """
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or len(layers) == 0:
-            raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(n, int) and n > 0 for n in layers):
             raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
@@ -23,8 +26,15 @@ class DeepNeuralNetwork:
         self.weights = {}
 
         for l in range(self.L):
-            layer_size = layers[l]
-            prev_size = nx if l == 0 else layers[l - 1]
-            self.weights['W' + str(l + 1)] = (np.random.randn(layer_size, prev_size) *
-                                              np.sqrt(2 / prev_size))
-            self.weights['b' + str(l + 1)] = np.zeros((layer_size, 1))
+            if not isinstance(layers[l], int) or layers[l] <= 0:
+                raise TypeError("layers must be a list of positive integers")
+
+            # Determine input size for the current layer
+            # Layer 1 (index 0) takes nx as input; others take layers[l-1]
+            n_prev = nx if l == 0 else layers[l - 1]
+            n_curr = layers[l]
+
+            # He et al. initialization
+            self.weights[f'W{l + 1}'] = np.random.randn(n_curr, n_prev) * np.sqrt(2 / n_prev)
+            # Bias initialization to 0
+            self.weights[f'b{l + 1}'] = np.zeros((n_curr, 1))
