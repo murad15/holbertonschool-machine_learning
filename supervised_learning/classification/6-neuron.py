2e82d8f88484e04be2a5300d5dcfddac6ac575e3
murad15 <muradis1503@gmail.com>
2026-03-29 21:39:44 +0000

    gitpusher

---
+++ supervised_learning/classification/6-neuron.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+"""qwdqd qwq d qwd qd qwd qwd wqd"""
+
+
+import numpy as np
+
+
+class Neuron:
+    """Class that defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """Initialize the neuron qwdq wq dasd asd"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        # Private attributes
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for weights dasdas ad as asd asd a"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for bias sdf sdf sdf sdfqw"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for activated output dqdwd qd """
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculates dfs df sfs fdsfd"""
+        Z = np.dot(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """Calculates cost of neural net"""
+        m = Y.shape[1]
+        cost = -np.sum(Y * np.log(A) +
+                       (1 - Y) * np.log(1.0000001 - A)) / m
+        return cost
+
+    def evaluate(self, X, Y):
+        """Calculates cost of neural net"""
+        A = self.forward_prop(X)
+        prediction = (A >= 0.5).astype(int)
+        cost = self.cost(Y, A)
+        return prediction, cost
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """Calculates cost of neural net"""
+        m = Y.shape[1]
+        dZ = A - Y
+        dW = np.dot(dZ, X.T) / m
+        db = np.sum(dZ) / m
+        self.__W = self.__W - alpha * dW
+        self.__b = self.__b - alpha * db
+
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """Calculates cost of neural net"""
+        if not isinstance(iterations, int):
+            raise TypeError("iterations must be an integer")
+        if iterations <= 0:
+            raise ValueError("iterations must be a positive integer")
+        if not isinstance(alpha, float):
+            raise TypeError("alpha must be a float")
+        if alpha <= 0:
+            raise ValueError("alpha must be positive")
+
+        for i in range(iterations):
+            A = self.forward_prop(X)
+            self.gradient_descent(X, Y, A, alpha)
+
+        return self.evaluate(X, Y)
