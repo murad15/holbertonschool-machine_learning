78a6a841fdbaea0407ac7610e5286ffbee60b549
murad15 <muradis1503@gmail.com>
2026-04-05 19:06:30 +0000

    gitpusher

---
+++ supervised_learning/cnn/2-conv_backward.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""Performs back propagation over a convolutional layer"""
+
+
+import numpy as np
+
+
+def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
+    """Performs backpropagation over a convolutional layer"""
+
+    m, h_new, w_new, c_new = dZ.shape
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw, c_prev, c_new = W.shape
+    sh, sw = stride
+
+    if padding == "same":
+        ph = ((h_prev - 1) * sh + kh - h_prev) // 2
+        pw = ((w_prev - 1) * sw + kw - w_prev) // 2
+    else:  # valid
+        ph, pw = 0, 0
+
+    A_prev_pad = np.pad(
+        A_prev,
+        ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+        mode="constant"
+    )
+
+    dA_prev_pad = np.zeros_like(A_prev_pad)
+    dW = np.zeros_like(W)
+    db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
+
+    for i in range(m):
+        a_prev = A_prev_pad[i]
+        da_prev = dA_prev_pad[i]
+
+        for h in range(h_new):
+            for w in range(w_new):
+                for c in range(c_new):
+
+                    vert_start = h * sh
+                    vert_end = vert_start + kh
+                    horiz_start = w * sw
+                    horiz_end = horiz_start + kw
+
+                    a_slice = a_prev[
+                        vert_start:vert_end,
+                        horiz_start:horiz_end,
+                        :
+                    ]
+
+                    da_prev[
+                        vert_start:vert_end,
+                        horiz_start:horiz_end,
+                        :
+                    ] += W[:, :, :, c] * dZ[i, h, w, c]
+
+                    dW[:, :, :, c] += a_slice * dZ[i, h, w, c]
+
+        dA_prev_pad[i] = da_prev
+
+    if padding == "same":
+        dA_prev = dA_prev_pad[:, ph:-ph, pw:-pw, :]
+    else:
+        dA_prev = dA_prev_pad
+
+    return dA_prev, dW, db
