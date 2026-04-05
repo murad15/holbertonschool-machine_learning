799ca4ec52339c41a1b2b997d6e180f3835236c3
murad15 <muradis1503@gmail.com>
2026-04-05 19:10:53 +0000

    gitpusher

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -14,19 +14,20 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     sh, sw = stride
 
     if padding == "same":
-        ph = int(((h_prev - 1) * sh + kh - h_prev) / 2)
-        pw = int(((w_prev - 1) * sw + kw - w_prev) / 2)
+        ph = ((h_prev - 1) * sh + kh - h_prev) // 2
+        pw = ((w_prev - 1) * sw + kw - w_prev) // 2
     else:
         ph, pw = 0, 0
 
     A_prev_pad = np.pad(
         A_prev,
         ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-        mode='constant'
+        mode="constant"
     )
 
     dA_prev_pad = np.zeros_like(A_prev_pad)
     dW = np.zeros_like(W)
+
     db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
 
     for i in range(m):
@@ -35,20 +36,24 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
 
         for h in range(h_new):
             for w in range(w_new):
+                vert_start = h * sh
+                vert_end = vert_start + kh
+                horiz_start = w * sw
+                horiz_end = horiz_start + kw
+
                 for c in range(c_new):
 
-                    vert_start = h * sh
-                    vert_end = vert_start + kh
-                    horiz_start = w * sw
-                    horiz_end = horiz_start + kw
+                    a_slice = a_prev[
+                        vert_start:vert_end,
+                        horiz_start:horiz_end,
+                        :
+                    ]
 
-                    a_slice = a_prev[vert_start:vert_end,
-                                     horiz_start:horiz_end,
-                                     :]
-
-                    da_prev[vert_start:vert_end,
-                            horiz_start:horiz_end,
-                            :] += W[:, :, :, c] * dZ[i, h, w, c]
+                    da_prev[
+                        vert_start:vert_end,
+                        horiz_start:horiz_end,
+                        :
+                    ] += W[:, :, :, c] * dZ[i, h, w, c]
 
                     dW[:, :, :, c] += a_slice * dZ[i, h, w, c]
 
