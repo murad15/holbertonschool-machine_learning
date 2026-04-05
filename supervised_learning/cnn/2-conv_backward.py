09da9f12996f2b72d1e6a510180f1fa5b97845c9
murad15 <muradis1503@gmail.com>
2026-04-05 19:12:57 +0000

    gitpusher

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Performs back propagation over a convolutional layer"""
 
-
 import numpy as np
 
 
@@ -22,7 +21,7 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     A_prev_pad = np.pad(
         A_prev,
         ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-        mode="constant"
+        mode='constant'
     )
 
     dA_prev_pad = np.zeros_like(A_prev_pad)
@@ -31,9 +30,6 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
 
     for i in range(m):
-        a_prev = A_prev_pad[i]
-        da_prev = dA_prev_pad[i]
-
         for h in range(h_new):
             for w in range(w_new):
                 vert_start = h * sh
@@ -43,24 +39,27 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
 
                 for c in range(c_new):
 
-                    a_slice = a_prev[
-                        vert_start:vert_end,
-                        horiz_start:horiz_end,
-                        :
-                    ]
+                    a_slice = A_prev_pad[i,
+                                         vert_start:vert_end,
+                                         horiz_start:horiz_end,
+                                         :]
 
-                    da_prev[
-                        vert_start:vert_end,
-                        horiz_start:horiz_end,
-                        :
-                    ] += W[:, :, :, c] * dZ[i, h, w, c]
+                    dA_prev_pad[i,
+                                vert_start:vert_end,
+                                horiz_start:horiz_end,
+                                :] += W[:, :, :, c] * dZ[i, h, w, c]
 
                     dW[:, :, :, c] += a_slice * dZ[i, h, w, c]
 
-        dA_prev_pad[i] = da_prev
-
     if padding == "same":
-        dA_prev = dA_prev_pad[:, ph:-ph, pw:-pw, :]
+        if ph > 0 and pw > 0:
+            dA_prev = dA_prev_pad[:, ph:-ph, pw:-pw, :]
+        elif ph > 0:
+            dA_prev = dA_prev_pad[:, ph:-ph, :, :]
+        elif pw > 0:
+            dA_prev = dA_prev_pad[:, :, pw:-pw, :]
+        else:
+            dA_prev = dA_prev_pad
     else:
         dA_prev = dA_prev_pad
 
