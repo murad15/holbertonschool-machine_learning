4fe5ecc979fb4e71a613f3e1648ad300cac0d43f
murad15 <muradis1503@gmail.com>
2026-04-05 18:22:53 +0000

    gitpusher

---
+++ supervised_learning/cnn/1-pool_forward.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Something that function does"""
+
+
+import numpy as np
+
+
+def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
+    """Something that function does"""
+
+    m, h, w, c = A_prev.shape
+    kh, kw = kernel_shape
+    sh, sw = stride
+
+    images
+    oh = (h - kh) // sh + 1
+    ow = (w - kw) // sw + 1
+
+    output = np.zeros((m, oh, ow, c))
+
+    for i in range(oh):
+        for j in range(ow):
+            img_slice = images[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :]
+            if mode == 'max':
+                output[:, i, j, :] = np.max(img_slice, axis=(1, 2))
+            else:
+                output[:, i, j, :] = np.mean(img_slice, axis=(1, 2))
+
+    return output
