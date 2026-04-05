7fc55c18fa93cbf664f34fa8bca2bc6582544be2
murad15 <muradis1503@gmail.com>
2026-04-05 19:18:18 +0000

    gitpusher

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -12,55 +12,44 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     kh, kw, c_prev, c_new = W.shape
     sh, sw = stride
 
+    # 1. Fixed Padding Calculation
     if padding == "same":
-        ph = ((h_prev - 1) * sh + kh - h_prev) // 2
-        pw = ((w_prev - 1) * sw + kw - w_prev) // 2
+        # Use ceil to ensure the padded volume is large enough
+        ph = int(np.ceil(((h_prev - 1) * sh + kh - h_prev) / 2))
+        pw = int(np.ceil(((w_prev - 1) * sw + kw - w_prev) / 2))
     else:
         ph, pw = 0, 0
 
+    # Pad the input and initialize gradient buffers
     A_prev_pad = np.pad(
         A_prev,
         ((0, 0), (ph, ph), (pw, pw), (0, 0)),
         mode='constant'
     )
-
     dA_prev_pad = np.zeros_like(A_prev_pad)
     dW = np.zeros_like(W)
-
     db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
 
     for i in range(m):
         for h in range(h_new):
             for w in range(w_new):
+                # Calculate corners
                 vert_start = h * sh
                 vert_end = vert_start + kh
                 horiz_start = w * sw
                 horiz_end = horiz_start + kw
 
                 for c in range(c_new):
+                    # Slicing the padded input
+                    a_slice = A_prev_pad[i, vert_start:vert_end, horiz_start:horiz_end, :]
 
-                    a_slice = A_prev_pad[i,
-                                         vert_start:vert_end,
-                                         horiz_start:horiz_end,
-                                         :]
-
-                    dA_prev_pad[i,
-                                vert_start:vert_end,
-                                horiz_start:horiz_end,
-                                :] += W[:, :, :, c] * dZ[i, h, w, c]
-
+                    # Update gradients
+                    # The ValueError happens here if the slice is smaller than the filter
+                    dA_prev_pad[i, vert_start:vert_end, horiz_start:horiz_end, :] += W[:, :, :, c] * dZ[i, h, w, c]
                     dW[:, :, :, c] += a_slice * dZ[i, h, w, c]
 
-    if padding == "same":
-        if ph > 0 and pw > 0:
-            dA_prev = dA_prev_pad[:, ph:-ph, pw:-pw, :]
-        elif ph > 0:
-            dA_prev = dA_prev_pad[:, ph:-ph, :, :]
-        elif pw > 0:
-            dA_prev = dA_prev_pad[:, :, pw:-pw, :]
-        else:
-            dA_prev = dA_prev_pad
-    else:
-        dA_prev = dA_prev_pad
+    # 2. Fixed Cropping Logic
+    # Slicing from ph to ph + h_prev handles ph=0 correctly
+    dA_prev = dA_prev_pad[:, ph:ph + h_prev, pw:pw + w_prev, :]
 
     return dA_prev, dW, db
