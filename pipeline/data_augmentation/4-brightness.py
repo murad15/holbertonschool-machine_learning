5d204a0bdd7d16af4571ea70d923f04d8fd686ca
murad15 <muradis1503@gmail.com>
2026-04-08 12:42:45 +0000

    gitpusher

--- pipeline/data_augmentation/4-brightness.py
+++ pipeline/data_augmentation/4-brightness.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
-"""This function does something interesting""""
-
+"""Randomly change image brightness using TensorFlow."""
 
 import tensorflow as tf
 
 
 def change_brightness(image, max_delta):
-    """This function does something interesting""""
+    """Randomly changes the brightness of an image.
 
+    Args:
+        image: A 3D tf.Tensor containing the image.
+        max_delta: Maximum amount to brighten or darken the image.
+
+    Returns:
+        The altered image as a tf.Tensor.
+    """
     return tf.image.random_brightness(image, max_delta)
