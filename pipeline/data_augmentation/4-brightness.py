83938fb05039e5096f826685493b4334d34ee39c
murad15 <muradis1503@gmail.com>
2026-04-08 12:36:25 +0000

    gitpusher

--- pipeline/data_augmentation/4-brightness.py
+++ pipeline/data_augmentation/4-brightness.py
@@ -8,5 +8,5 @@ import tensorflow as tf
 def change_brightness(image, max_delta):
     """This function does something interesting""""
 
-    bright = tf.image.adjust_brightness(image, max_delta)
+    bright = tf.image.random_brightness(image, max_delta)
     return bright
