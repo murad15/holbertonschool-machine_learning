634445032fb94000282b6e1f93342fc7c1c94737
murad15 <muradis1503@gmail.com>
2026-04-08 12:47:59 +0000

    gitpusher

--- pipeline/data_augmentation/5-hue.py
+++ pipeline/data_augmentation/5-hue.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 
 
 def change_hue(image, max_delta):
-    """This function does something interesting""""
+    """This function does something interesting"""
 
     hue = tf.image.adjust_hue(image, delta, name=None)
     return hue
