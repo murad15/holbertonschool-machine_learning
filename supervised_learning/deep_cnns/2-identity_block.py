b5db5e3889b220d8f9098ae6711384505419f43b
Murad Isgandarov <71070413+murad15@users.noreply.github.com>
2026-04-19 22:28:27 +0400

    Replace Activation layers with ReLU layers

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -13,21 +13,25 @@ def identity_block(A_prev, filters):
 
     shortcut = A_prev
 
+    # 1x1
     X = K.layers.Conv2D(F11, (1, 1), padding='same',
                         kernel_initializer=initializer)(A_prev)
     X = K.layers.BatchNormalization(axis=-1)(X)
-    X = K.layers.Activation('relu')(X)
+    X = K.layers.ReLU()(X)
 
+    # 3x3
     X = K.layers.Conv2D(F3, (3, 3), padding='same',
                         kernel_initializer=initializer)(X)
     X = K.layers.BatchNormalization(axis=-1)(X)
-    X = K.layers.Activation('relu')(X)
+    X = K.layers.ReLU()(X)
 
+    # 1x1
     X = K.layers.Conv2D(F12, (1, 1), padding='same',
                         kernel_initializer=initializer)(X)
     X = K.layers.BatchNormalization(axis=-1)(X)
 
+    # Add
     X = K.layers.Add()([X, shortcut])
-    X = K.layers.Activation('relu')(X)
+    X = K.layers.ReLU()(X)
 
     return X
