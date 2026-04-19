34470d16a5b37bb4c61f0621197f82038d561bbf
Murad Isgandarov <71070413+murad15@users.noreply.github.com>
2026-04-19 22:39:19 +0400

    Update 2-identity_block.py

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -17,13 +17,13 @@ def identity_block(A_prev, filters):
     X = K.layers.Conv2D(F11, (1, 1), padding='same',
                         kernel_initializer=initializer)(A_prev)
     X = K.layers.BatchNormalization(axis=-1)(X)
-    X = K.layers.ReLU()(X)
+    X = K.layers.Activation('relu')
 
     # 3x3
     X = K.layers.Conv2D(F3, (3, 3), padding='same',
                         kernel_initializer=initializer)(X)
     X = K.layers.BatchNormalization(axis=-1)(X)
-    X = K.layers.ReLU()(X)
+    X = K.layers.Activation('relu')
 
     # 1x1
     X = K.layers.Conv2D(F12, (1, 1), padding='same',
@@ -32,6 +32,6 @@ def identity_block(A_prev, filters):
 
     # Add
     X = K.layers.Add()([X, shortcut])
-    X = K.layers.ReLU()(X)
+    X = K.layers.Activation('relu')
 
     return X
