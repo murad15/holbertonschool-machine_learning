6b98ca7550e292e69c188d5e49a08caf7165ced2
Murad Isgandarov <71070413+murad15@users.noreply.github.com>
2026-05-24 22:27:12 +0400

    Create 0-initialize.py

---
+++ unsupervised_learning/clustering/0-initialize.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def initialize(X, k):
+    """Initializes cluster centroids for K-means."""
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        return None
+
+    if type(k) is not int or k <= 0:
+        return None
+
+    return np.random.uniform(
+        X.min(axis=0),
+        X.max(axis=0),
+        size=(k, X.shape[1])
+    )
