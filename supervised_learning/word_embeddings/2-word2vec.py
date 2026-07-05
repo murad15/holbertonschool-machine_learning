1e6cfdc1157772d18913ae703d473f8be4b72421
Murad Isgandarov <71070413+murad15@users.noreply.github.com>
2026-07-05 20:03:54 +0400

    Update 2-word2vec.py

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -3,8 +3,7 @@
 word 2 vec
 """
 
-from gensim.models import Word2Vec
-
+import gensim
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5, negative=5, 
                    cbow=True, epochs=5, seed=0, workers=1):
