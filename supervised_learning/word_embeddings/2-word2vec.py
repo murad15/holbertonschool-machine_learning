a25365bab86b405f92256a912ea6e76773b1b215
Murad Isgandarov <71070413+murad15@users.noreply.github.com>
2026-07-05 21:42:32 +0400

    Update 2-word2vec.py

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -26,21 +26,15 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
         The trained Word2Vec model
     """
     model = gensim.models.Word2Vec(
+        sentences,
         vector_size=vector_size,
         min_count=min_count,
         window=window,
         negative=negative,
         sg=0 if cbow else 1,
+        epochs=epochs,
         seed=seed,
         workers=workers
-    )
-
-    model.build_vocab(sentences)
-
-    model.train(
-        sentences,
-        total_examples=model.corpus_count,
-        epochs=epochs
     )
 
     return model
