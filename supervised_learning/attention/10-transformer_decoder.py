89006e5ea36238644e127c40ef468f8d76adbbac
Murad Isgandarov <71070413+murad15@users.noreply.github.com>
2026-07-12 23:27:55 +0400

    Create 10-transformer_decoder.py

---
+++ supervised_learning/attention/10-transformer_decoder.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+"""Transformer decoder module."""
+
+import tensorflow as tf
+
+positional_encoding = __import__('4-positional_encoding').positional_encoding
+
+DecoderBlock = __import__('8-transformer_decoder_block').DecoderBlock
+
+
+class Decoder(tf.keras.layers.Layer):
+    """Creates the decoder for a Transformer."""
+
+    def __init__(
+        self,
+        N,
+        dm,
+        h,
+        hidden,
+        target_vocab,
+        max_seq_len,
+        drop_rate=0.1
+    ):
+        """
+        Initialize the Transformer decoder.
+
+        Args:
+            N: Number of decoder blocks.
+            dm: Dimensionality of the model.
+            h: Number of attention heads.
+            hidden: Number of units in the fully connected layer.
+            target_vocab: Size of the target vocabulary.
+            max_seq_len: Maximum target sequence length.
+            drop_rate: Dropout rate.
+        """
+        super().__init__()
+
+        self.N = N
+        self.dm = dm
+
+        self.embedding = tf.keras.layers.Embedding(
+            input_dim=target_vocab,
+            output_dim=dm
+        )
+
+        self.positional_encoding = positional_encoding(
+            max_seq_len,
+            dm
+        )
+
+        self.blocks = [
+            DecoderBlock(dm, h, hidden, drop_rate)
+            for _ in range(N)
+        ]
+
+        self.dropout = tf.keras.layers.Dropout(drop_rate)
+
+    def call(
+        self,
+        x,
+        encoder_output,
+        training,
+        look_ahead_mask,
+        padding_mask
+    ):
+        """
+        Perform forward propagation through the decoder.
+
+        Args:
+            x: Tensor of shape (batch, target_seq_len) containing
+                target token indices.
+            encoder_output: Tensor of shape
+                (batch, input_seq_len, dm).
+            training: Boolean indicating whether the model is training.
+            look_ahead_mask: Mask for the first attention layer.
+            padding_mask: Mask for the second attention layer.
+
+        Returns:
+            Tensor of shape (batch, target_seq_len, dm).
+        """
+        target_seq_len = tf.shape(x)[1]
+
+        x = self.embedding(x)
+
+        x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
+
+        x += self.positional_encoding[:target_seq_len]
+
+        x = self.dropout(x, training=training)
+
+        for block in self.blocks:
+            x = block(
+                x,
+                encoder_output,
+                training,
+                look_ahead_mask,
+                padding_mask
+            )
+
+        return x
