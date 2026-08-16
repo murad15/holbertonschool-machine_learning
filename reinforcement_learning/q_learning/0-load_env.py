653ba6667627938bcee54ea2710dc4d41f1359e0
Murad Isgandarov <71070413+murad15@users.noreply.github.com>
2026-08-16 20:34:33 +0400

    Add render_mode parameter to load environment

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -10,5 +10,6 @@ def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
         "FrozenLake-v1",
         desc=desc,
         map_name=map_name,
-        is_slippery=is_slippery
+        is_slippery=is_slippery,
+        render_mode="ansi"
     ).unwrapped
