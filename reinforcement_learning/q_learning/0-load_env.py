85a986b0425a9dc8c6f315da881c4d6d358db775
Murad Isgandarov <71070413+murad15@users.noreply.github.com>
2026-08-16 20:24:43 +0400

    Update 0-load_env.py

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
-"""Module for loading the FrozenLake environment."""
+"""Module for loading a FrozenLake environment."""
 
 import gymnasium as gym
 
 
 def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
-    """Load and return a FrozenLake environment."""
-    if desc is None and map_name is None:
-        desc = gym.envs.toy_text.frozen_lake.generate_random_map(size=8)
-
+    """Load and return an unwrapped FrozenLake environment."""
     return gym.make(
         "FrozenLake-v1",
         desc=desc,
         map_name=map_name,
         is_slippery=is_slippery
-    )
+    ).unwrapped
